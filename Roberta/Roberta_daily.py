35e3654b73f39f5eb79b6cc2ae78b59ddb6c71a0
YYHBJ <something563214789@gmail.com>
2022-04-02 17:55:07 +0800

    0402

--- Roberta/Roberta_daily.py
+++ Roberta/Roberta_daily.py
@@ -21,7 +21,7 @@ test = pd.read_csv('csv/test.csv')
 
 
 le = preprocessing.LabelEncoder()
-le.fit(df['emotion'])
+le.fit(df['topic'])
 c = list(le.classes_)
 labels={}
 for idx, la in enumerate(c):
@@ -31,7 +31,7 @@ class Dataset(torch.utils.data.Dataset):
     
     def __init__(self, df):
 
-        self.labels = [torch.tensor(labels[label], dtype=torch.long) for label in df['emotion']]
+        self.labels = [torch.tensor(labels[label], dtype=torch.long) for label in df['topic']]
         self.texts  = [tokenizer(text, 
                                padding='max_length', max_length = 512, truncation=True,
                                 return_tensors="pt") for text in df['text']]
@@ -60,14 +60,14 @@ class Dataset(torch.utils.data.Dataset):
 
 class Classifier(nn.Module):
     
-    def __init__(self, dropout=0.2):
+    def __init__(self, dropout=0.1):
 
         super(Classifier, self).__init__()
 
         self.bert = bert
         self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(1024, 7)
-        # self.linear2 = nn.Linear(512, 8)
+        self.linear = nn.Linear(1024, 10)
+        # self.linear2 = nn.Linear(256, 10)
         self.relu = nn.ReLU()
 
     def forward(self, input_id, mask):
@@ -80,6 +80,28 @@ class Classifier(nn.Module):
 
         return final_layer
     
+class Act_Classifier(nn.Module):
+    
+    def __init__(self, dropout=0.1):
+
+        super(Act_Classifier, self).__init__()
+
+        self.bert = bert
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(1024, 4)
+        # self.linear2 = nn.Linear(256, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        # linear_output2 = self.linear2(linear_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+        
 def train(model, train_data, val_data, learning_rate, epochs):
     
     train, val = Dataset(train_data), Dataset(val_data)
@@ -144,7 +166,7 @@ def train(model, train_data, val_data, learning_rate, epochs):
             print(
                 f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} | Train Accuracy: {total_acc_train / len(train_data): .3f} | Val Loss: {total_loss_val / len(val_data): .3f} | Val Accuracy: {total_acc_val / len(val_data): .3f}')
     
-    torch.save(model,'models/model_roberta_1.pt')  
+    torch.save(model,'models/model_roberta_topic2.pt')  
                      
 def evaluate(model, test_data):
     
@@ -175,16 +197,16 @@ def evaluate(model, test_data):
     
     print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
     
-np.random.seed(2019)
+np.random.seed(112)
 print(len(df),len(val), len(test))
 
 
 """
 TRAIN
 """
-EPOCHS = 7
+EPOCHS = 10
 model = Classifier()
-LR = 1e-7
+LR = 5e-7
               
 train(model, df, val, LR, EPOCHS)
 evaluate(model, test)
